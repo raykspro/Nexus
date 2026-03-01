d206c5d0bcf7df32d460cc0a1578b0eae0f4db8e
raykspro <rayanfelipe9882@gmail.com>
2026-02-28 23:33:14 -0300

    Modify base path and add build settings
    Updated base path for GitHub Pages and added build configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ezwallet/", // Essencial para o link do GitHub funcionar
+  base: "/EZwallet/", // Vital para o GitHub Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@shared": path.resolve(__dirname, "./shared"),
     },
   },
-})
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  }
+});
