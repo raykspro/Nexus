bf4039e5aae898fd5742b5ec589ec8a3d6db8014
raykspro <rayanfelipe9882@gmail.com>
2026-02-28 22:56:20 -0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
