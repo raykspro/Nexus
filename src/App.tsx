ce2ed5a94a946c7c9c64e3ba6ec6d72d71045550
raykspro <rayanfelipe9882@gmail.com>
2026-02-28 23:48:42 -0300

    Refactor App component and add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { Switch, Route } from "wouter";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
 import Dashboard from "./pages/dashboard";
 import Transactions from "./pages/transactions";
+import Layout from "./components/layout";
+
+function Router() {
+  return (
+    <Layout>
+      <Switch>
+        {/* As rotas agora conversam com o link do GitHub Pages do Senhor */}
+        <Route path="/" component={Dashboard} />
+        <Route path="/transactions" component={Transactions} />
+        <Route>404 - Mestre, esta página não existe.</Route>
+      </Switch>
+    </Layout>
+  );
+}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Switch>
-        <Route path="/ezwallet/" component={Dashboard} />
-        <Route path="/ezwallet/transactions" component={Transactions} />
-        <Route>404 - Página não encontrada</Route>
-      </Switch>
+      <Router />
     </QueryClientProvider>
   );
 }
