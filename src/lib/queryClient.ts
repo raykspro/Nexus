fa00e3299e3ff2fc24075054c4647038713362c5
raykspro <rayanfelipe9882@gmail.com>
2026-02-28 23:49:53 -0300

    Refactor queryClient to use default configuration
    Simplified queryClient initialization by removing default options.

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,10 +1,2 @@
 import { QueryClient } from "@tanstack/react-query";
-
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: false,
-    },
-  },
-});
+export const queryClient = new QueryClient();
