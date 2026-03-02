b3201778ded8a89c55db602df03b87c8ee911604
raykspro <rayanfelipe9882@gmail.com>
2026-03-02 06:39:02 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 import path from "path";
+
+// No padrão "type: module", precisamos recriar o __dirname para o Linux do GitHub
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  // FUNDAMENTAL: Alterado para /Nexus/ para coincidir com o novo repositório
+  // Essencial para o GitHub Pages encontrar o Nexus
   base: "/Nexus/", 
   resolve: {
     alias: {
