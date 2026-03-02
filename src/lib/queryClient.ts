99112c64f548687b247ba16895987b814cb34152
raykspro <rayanfelipe9882@gmail.com>
2026-03-02 06:32:46 -0300

    Atualizar o queryClient.ts

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,10 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
 
+// Este é o cérebro que coordena as informações da Nexus
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-      retry: false,
+      refetchOnWindowFocus: false, // Evita recargas desnecessárias quando o Senhor volta para a aba
+      retry: false, // Se houver erro de leitura, o sistema reporta ao Senhor imediatamente
     },
   },
 });
