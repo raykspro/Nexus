cd194df0a9f6f7a1f5ca91f925f58148cf86c121
raykspro <rayanfelipe9882@gmail.com>
2026-03-02 06:20:48 -0300

    Atualizar o main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // ESTA LINHA É VITAL PARA AS CORES APARECEREM
+import './index.css'; 
 
-// O robô do GitHub precisa encontrar o elemento "root" que o Senhor criou na index.html
 const rootElement = document.getElementById('root');
 
-if (!rootElement) {
-  throw new Error('Mestre, não encontrei o elemento root no seu index.html. Verifique o arquivo!');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  // Erro silencioso no console para não quebrar o build, mas alertar o desenvolvedor
+  console.error('Elemento root não encontrado.');
 }
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
