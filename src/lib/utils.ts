0109ab6784f5a55f937f09f567540e93e7554ccb
raykspro <rayanfelipe9882@gmail.com>
2026-03-04 21:22:51 -0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Função CN (Class Name Merger):
+ * Combina classes do Tailwind e resolve conflitos de estilo automaticamente.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
