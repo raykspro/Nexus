9c35397783428cef8ffa4240b736fd051accd33c
raykspro <rayanfelipe9882@gmail.com>
2026-03-04 22:28:12 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Layout } from "@/components/layout"; // Usando o alias @ que forjamos
+import Layout from "@/components/layout";
 import Dashboard from "@/pages/dashboard";
 import Transactions from "@/pages/transactions";
 
